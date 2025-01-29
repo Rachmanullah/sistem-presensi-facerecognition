c725651b1b36ea90d7656f6adca179166d3a3e6b
Rachmanullah <rachmanullah1@gmail.com>
2025-01-30 00:19:13 +0700

    fix conflix absensi

--- backend/route/checkPresensi.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { absensiController } = require('../controller');
-
-const router = express.Router();
-router.get('/', absensiController.);
-
-module.exports = router;
